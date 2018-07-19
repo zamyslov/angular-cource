a596c3c739185499084d944ba216602cd47c6ea2
zamyslov@gmail.com <194156567>
2018-07-19 15:22:47 +0300

    lesson 15

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,9 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css',],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class CarsComponent {
 

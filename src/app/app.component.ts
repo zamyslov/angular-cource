f368a123ab000d4cc31959a6c283150508dd58ff
zamyslov@gmail.com <zamyslov@gmail.com>
2018-07-21 15:48:22 +0300

    lesson 28

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import {Component} from '@angular/core';
   template: `
     <div class="col-xs-4 col-5">
       <input class="form-control" [(ngModel)]="searchCar">
+      <button class="btn-primary" (click)="addCar()">Добавить</button>
       <hr>
       <ul class="list-group">
-        <li *ngFor="let car of cars | carFilter:searchCar:'description';let i = index"
+        <li *ngFor="let car of cars | carFilter:searchCar:'name';let i = index"
             class="list-group-item"><b>{{i + 1}}</b> {{car.name}} <i>{{car.description}}</i></li>
       </ul>
     </div>
@@ -21,4 +22,10 @@ export class AppComponent {
     {name: 'BMW', description: 'car 3'},
     {name: 'Lada', description: 'car 4'}
   ];
+
+  addCar() {
+    this.cars.push({
+      name: 'newCar', description: 'car'
+    });
+  }
 }
--- src/app/car-filter.pipe.ts
+++ src/app/car-filter.pipe.ts
@@ -1,7 +1,8 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'carFilter'
+  name: 'carFilter',
+  pure: false
 })
 export class CarFilterPipe implements PipeTransform {
 

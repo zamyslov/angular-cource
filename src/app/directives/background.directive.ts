caeb549a90230924282fdc71192a0f5b278545e0
zamyslov@gmail.com <zamyslov@gmail.com>
2018-07-21 14:08:00 +0300

    lesson 23

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,10 +1,12 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 
 export class BackgroundDirective implements OnInit {
+  @Input('appBackground') hoverColor = 'green';
+  @Input() defaultColor = 'transparent';
   @HostBinding('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
@@ -12,16 +14,17 @@ export class BackgroundDirective implements OnInit {
   }
 
   ngOnInit(): void {
+    this.background = this.defaultColor;
   }
 
   @HostListener('mouseenter') onMouseEnter() {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
-    this.background = 'red';
+    this.background = this.hoverColor;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-    this.background = 'transparent';
+    this.background = this.defaultColor;
   }
 
 }

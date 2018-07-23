7667f50f9926d4dafe7caad7d8dfeac901cc2429
zamyslov@gmail.com <194156567>
2018-07-23 13:15:52 +0300

    lesson 30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,20 @@
 import {Component} from '@angular/core';
-import 'rxjs';
-import { Observable } from 'rxjs';
-import { of } from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="col-xs-4 col-5">
-      <h1>{{asyncTitle}}</h1>
-      <input class="form-control" [(ngModel)]="searchCar">
-      <button class="btn-primary" (click)="addCar()">Добавить</button>
-      <hr>
-      <ul class="list-group">
-        <li *ngFor="let car of cars | carFilter:searchCar:'name';let i = index"
-            class="list-group-item"><b>{{i + 1}}</b> {{car.name}} <i>{{car.description}}</i></li>
-      </ul>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  searchCar = '';
   cars = [
-    {name: 'Mazda', description: 'car 1'},
-    {name: 'Ford', description: 'car 2'},
-    {name: 'BMW', description: 'car 3'},
-    {name: 'Lada', description: 'car 4'}
+    {name: 'Mazda', isSold: false},
+    {name: 'Ford', isSold: false},
+    {name: 'BMW', isSold: false},
+    {name: 'Lada', isSold: false}
   ];
 
-  asyncTitle = Observable.of('Async').delay(3000);
-
-  addCar() {
+  addCarToList(carName: string) {
     this.cars.push({
-      name: 'newCar', description: 'car'
+      name: carName, isSold: false
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {PowPipe} from './pow.pipe';
 import { CarFilterPipe } from './car-filter.pipe';
+import { CarComponent } from './car/car.component';
+import { CarAddComponent } from './car-add/car-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PowPipe,
-    CarFilterPipe
+    CarFilterPipe,
+    CarComponent,
+    CarAddComponent
   ],
   imports: [
     BrowserModule, FormsModule

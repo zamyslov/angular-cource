c89469fe7949b5f967bc743f17221c574de1ca4d
zamyslov@gmail.com <194156567>
2018-07-25 12:58:23 +0300

    lesson 39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -6,6 +6,8 @@ import {NgForm} from '@angular/forms';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
+
   answers = [
     {type: 'yes', text: 'Да'},
     {type: 'no', text: 'Нет'}
@@ -14,9 +16,30 @@ export class AppComponent {
   defaultAnswer = 'no';
   defaultCountry = 'ukr';
 
+  formData = {};
+  isSubmited = false;
 
-  submitForm(form: NgForm) {
+  submitForm() {
+    this.isSubmited = true;
+    this.formData = this.form.value;
+    this.form.reset();
+  }
 
+  addRandEmail() {
+    const randEmail = 'a@a';
+    // this.form.setValue({
+    //   user: {
+    //     pass: '',
+    //     email: randEmail
+    //   },
+    //   country: '',
+    //   answer: ''
+    // });
+    this.form.form.patchValue({
+      user: {
+        email: randEmail
+      }
+    });
   }
 
 }

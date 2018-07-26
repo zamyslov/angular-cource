b440b2d7695f382ab4573a5e9f1c65542ed1cc85
zamyslov@gmail.com <zamyslov@gmail.com>
2018-07-26 22:18:27 +0300

    lesson 47

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import {Component} from '@angular/core';
 import {CarsService} from './cars.service';
 
+interface Cars {
+  name: string;
+  color: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
 
-  cars = [];
+  cars: Cars[] = [];
 
   constructor(private carService: CarsService) {
   }
 
   loadCars() {
-    this.carService.getCars().subscribe(response => {
-      console.log(response);
+    this.carService.getCars().subscribe((response: Cars[]) => {
+      this.cars = response;
     });
   }
 

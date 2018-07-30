5b7a851c2de6a5e46efeeb04a6d40fbd6f8cc2f4
zamyslov@gmail.com <194156567>
2018-07-30 10:56:35 +0300

    lesson 59

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,19 +5,27 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
+      year: 2017,
+      color: 'red',
       id: 1
     },
     {
       name: 'Audi',
+      year: 2010,
+      color: 'blue',
       id: 2
     },
     {
       name: 'BMW',
+      year: 2016,
+      color: 'white',
       id: 3
     },
     {
       name: 'Mercedes',
+      year: 2014,
+      color: 'yellow',
       id: 4
     }
-  ]
+  ];
 }

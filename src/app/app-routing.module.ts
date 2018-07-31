3bf6c90be72b0d44d4d753080dfa1d18a37ae6a9
zamyslov@gmail.com <194156567>
2018-07-31 13:09:38 +0300

    lesson 66

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 
 const appRoutes: Routes = [
@@ -8,7 +8,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
